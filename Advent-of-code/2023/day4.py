6ef79a562f4f6b918890933240cd76de0d4a46e9
Iris Burmistrov <irisburmi@gmail.com>
2024-07-06 19:21:33 +0300

    AOT 2023 day 4: solve.

--- Advent-of-code/2023/day4.py
+++ Advent-of-code/2023/day4.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+# Part 1 #
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def calculate_score_from_card(winning_numbers, elf_numbers):
+    score = 0
+    for number in elf_numbers:
+        if number in winning_numbers:
+            score = 1 if score < 1 else score * 2
+    return score
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def print_cards_score():
+    with open("input.txt", "r") as input_file:
+        score = 0
+        lines = input_file.readlines()
+        for line in lines:
+            winning_numbers = line.split(":")[1].split("|")[0].split()
+            elf_numbers = line.split(":")[1].split("|")[1][:-1].split()
+            score += calculate_score_from_card(winning_numbers, elf_numbers)
+    print(score)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Part 2 #
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def calculate_card_stats(winning_numbers, elf_numbers):
+    matching_cards = 0
+    for number in elf_numbers:
+        if number in winning_numbers:
+            matching_cards += 1
+    return matching_cards
+
+
+def print_cards_score_with_copies():
+    with open("input.txt", "r") as input_file:
+        matches = []
+        copies_count = [1] * 196
+        lines = input_file.readlines()
+        for i, line in enumerate(lines):
+            winning_numbers = line.split(":")[1].split("|")[0].split()
+            elf_numbers = line.split(":")[1].split("|")[1][:-1].split()
+            matching_cards = calculate_card_stats(winning_numbers, elf_numbers)
+            matches.append(matching_cards)
+            for j in range(matching_cards):
+                if i+j+1 < 196:
+                    copies_count[i+j+1] += copies_count[i]
+    print(sum(copies_count))
+
+
+if __name__ == "__main__":
+    print_cards_score_with_copies()
