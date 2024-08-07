ecb5667e6b23e944cac3c6690edcf5742d5ab2ae
irisbur <irisburmi@gmail.com>
2024-08-07 11:02:03 +0300

    LeetCode 21: improve solution.

--- LeetCode/Merge Two Sorted Lists.py
+++ LeetCode/Merge Two Sorted Lists.py
@@ -1,10 +1,35 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 # There are several variant of this question, this one is problem 21 in LeetCode.
+
+
+# Nicer solution:
+def merge_two_lists_neater(list1, list2):
+    dummy = ListNode()
+    curr = dummy
+
+    while list1 and list2:
+        if list1.val > list2.val:
+            curr.next = list2
+            list2 = list2.next
+        else:
+            curr.next = list1
+            list1 = list1.next
+        curr = curr.next
+
+    if list1:
+        curr.next = list1
+    if list2:
+        curr.next = list2
+
+    return dummy.next
+
+
+# inital draft.
 def merge_two_lists(list1, list2):
     if list1 is None:
         return list2
