4865ad25c5848f290f989be8b1a3101644746aec
irisbur <irisburmi@gmail.com>
2024-08-17 11:15:42 +0300

    RegEx: initial commit.

---
+++ Others/RegEx.py
@@ -0,0 +1,7 @@
+
+# A place for all RegEx types of questions.
+
+# given a string that contains only a's and b's,
+# write a regex expression to return true when a string starts and end with the same letter.
+
+reg_pattern = '^[a-b]$|^([a-b]).*\\1$'
