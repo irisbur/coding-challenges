109fdc6f4a1099cba3b4b4cae7db12cad80f936e
irisbur <irisburmi@gmail.com>
2024-08-20 18:00:05 +0300

    AOC 2023: better solution using BFS.

--- Advent-of-code/2023/day10.py
+++ Advent-of-code/2023/day10.py
@@ -7,9 +7,9 @@
 # . is ground; there is no pipe in this tile.
 # S is the starting position of the animal; there is a pipe on this
 # North -1, South 1, West -1 , East 1
-import math
+from collections import deque
 
-next_step_dict = {
+NEXT_STEP = {
     '|': [(1, 0), (-1, 0)], '-': [(0, 1), (0, -1)],
     'L': [(-1, 0), (0, 1)], 'J': [(-1, 0), (0, -1)],
     '7': [(0, -1), (1, 0)], 'F': [(1, 0), (0, 1)],
@@ -18,66 +18,31 @@ next_step_dict = {
 }
 
 
+def bfs_farthest_point(grid, start):
+    queue = deque([start])
+    visited = {start: 0}
+    max_dist = 0
+
+    while queue:
+        i, j = queue.popleft()
+
+        for di, dj in NEXT_STEP[grid[i][j]]:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and grid[ni][nj] != '.' and (ni, nj) not in visited:
+                back_dir = -di, -dj
+                if back_dir in NEXT_STEP[grid[ni][nj]]:
+                    visited[(ni, nj)] = visited[(i, j)] + 1
+                    queue.append((ni, nj))
+                    max_dist = max(max_dist, visited[(i, j)] + 1)
+
+    return max_dist
+
+
 def find_farthermost_point():
-    with (open("input.txt", "r") as input_file):
-        grid = [list(line[:-1]) for line in input_file.readlines()]
+    with open("input.txt", "r") as input_file:
+        grid = [list(line.strip()) for line in input_file.readlines()]
         start = find_start(grid)
-        steps = directions_from_start(grid, start)
-        prevs = [start] * len(steps)
-        steps_count = 1
-        while not did_finish_loop(grid, steps):
-            steps_count += 1
-            steps_that_has_next, nexts = find_next_steps(grid, prevs, steps)
-            prevs = steps_that_has_next
-            steps = nexts
-
-        return steps_count / 2
-
-
-def find_next_steps(grid, prevs, steps):
-    steps_that_has_next = []
-    nexts = []
-    for c, (i, j) in enumerate(steps):
-        initial_direction = prevs[c][0] - i, prevs[c][1] - j
-
-        if grid[i][j] in ['|', "-"]:
-            next_directions = [(k, l) for k, l in next_step_dict[grid[i][j]] if (k, l) != initial_direction]
-            next_direction = next_directions[0]
-            add_to_next_if_needed(steps_that_has_next, nexts, next_direction, i, j, grid)
-        else:
-            if initial_direction[0] == 0:
-                next_directions = [(k, l) for k, l in next_step_dict[grid[i][j]] if k != 0]
-                next_direction = next_directions[0]
-                add_to_next_if_needed(steps_that_has_next, nexts, next_direction, i, j, grid)
-            else:
-                next_directions = [(k, l) for k, l in next_step_dict[grid[i][j]] if k == 0]
-                next_direction = next_directions[0]
-                add_to_next_if_needed(steps_that_has_next, nexts, next_direction, i, j, grid)
-    return steps_that_has_next, nexts
-
-
-def add_to_next_if_needed(steps_that_has_next, nexts, next_direction, i, j, grid):
-    if 0 <= i + next_direction[0] < len(grid) and 0 <= j + next_direction[1] < len(grid[0]):
-        nexts.append((i + next_direction[0], j + next_direction[1]))
-        steps_that_has_next.append((i, j))
-
-
-def directions_from_start(grid, start):
-    steps_from_start = []
-    i, j = start
-    for k, l in next_step_dict['S']:
-        if 0 <= i + k < len(grid) and 0 <= j + l < len(grid[0]):
-            neighbor = grid[i + k][j + l]
-            if neighbor != '.':
-                steps_from_start.append((i + k, j + l))
-    return steps_from_start
-
-
-def did_finish_loop(grid, steps):
-    for i, j in steps:
-        if grid[i][j] == 'S':
-            return True
-    return False
+        return bfs_farthest_point(grid, start)
 
 
 def find_start(grid):
