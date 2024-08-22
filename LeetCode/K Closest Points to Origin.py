f91c362634b3d1c1b13d8fcc5d8b96ebcafae0c3
irisbur <irisburmi@gmail.com>
2024-08-22 21:01:03 +0300

    LeetCode: improve solution of k closest.

--- LeetCode/K Closest Points to Origin.py
+++ LeetCode/K Closest Points to Origin.py
@@ -10,28 +10,17 @@ def k_closest_with_sort(points: List[List[int]], k: int) -> List[List[int]]:
 def kClosest(points: List[List[int]], k: int) -> List[List[int]]:
     # idea: to each point calculate the distance of it from origin
     # then create a max heap for the k smallest values
-    distances_dict = {}
+    max_heap = []
+
     for x, y in points:
-        if x**2 + y ** 2 in distances_dict:
-            distances_dict[x**2 + y ** 2].append([x, y])
-        else:
-            distances_dict[x**2 + y ** 2] = [[x, y]]
+        d = x ** 2 + y ** 2
+        if len(max_heap) < k:
+            heapq.heappush(max_heap, (-1 * d, x, y))
+        elif d < -1 * max_heap[0][0]:
+            heapq.heapreplace(max_heap, (-1 * d, x, y))
+    result = [[x, y] for _, x, y in max_heap]
+    return result
 
-    k_closest = []
-    # push the distance value as the amount of items in that length
-    for i, d in enumerate(distances_dict.keys()):
-        l = len(distances_dict[d])
-        for _ in range(l):
-            if len(k_closest) < k:
-                heapq.heappush(k_closest, -1 *d)
-            elif d < -1* k_closest[0]:
-                heapq.heapreplace(k_closest, -1* d)
-
-    k_points = []
-    while k_closest:
-        points_to_add = distances_dict[-1 * heapq.heappop(k_closest)]
-        k_points.extend(points_to_add)
-    return k_points[::-1][:k]
 
 
 print(kClosest([[2,2],[2,2],[3,3],[2,-2],[1,1]],
