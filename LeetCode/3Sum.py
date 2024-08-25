0cc7066691a4ba32fdfcc4823b7f11aa151e7fab
irisbur <irisburmi@gmail.com>
2024-08-25 12:30:22 +0300

    LeetCode: improve 3Sum but ugly.

--- LeetCode/3Sum.py
+++ LeetCode/3Sum.py
@@ -6,13 +6,17 @@ def three_sum(nums: List[int]) -> List[List[int]]:
     nums_map = nums_to_map(nums)
 
     triplets = set()
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            diff = -1 * (nums[i] + nums[j])
-            if diff in nums_map:
-                idx = [k for k in nums_map[diff] if k != i and k != j]
-                for k in idx:
-                    triplets.add(tuple(sorted((nums[i], nums[j], nums[k]))))
+    for num1 in nums_map:
+        for num2 in nums_map:
+            if num1 == num2 and len(nums_map[num1]) < 2:
+                break
+
+            diff = -1 * (num1 + num2)
+            if num1 == num2 and num2 == diff and len(nums_map[num1]) > 2:
+                triplets.add(tuple(sorted((num1, num2, diff))))
+            elif diff in nums_map:
+                if (diff != num1 and diff != num2) or (diff == num1 and diff != num2 and len(nums_map[num1]) > 1) or (diff == num2 and diff != num1 and len(nums_map[num2]) > 1):
+                    triplets.add(tuple(sorted((num1, num2, diff))))
 
     return triplets
 
@@ -26,5 +30,4 @@ def nums_to_map(nums):
             nums_map[nums[i]] = [i]
     return nums_map
 
-
-print(three_sum([-1,0,1,2,-1,-4]))
+print(three_sum([-1,0,1,0]))
