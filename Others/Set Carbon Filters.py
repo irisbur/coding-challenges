f4520f256163785f8e5731937df0e73574f02d4c
irisbur <irisburmi@gmail.com>
2024-08-28 15:09:40 +0300

    Set Carbon Filters: my initial not efficient solution.

--- Others/Set Carbon Filters.py
+++ Others/Set Carbon Filters.py
@@ -26,4 +26,58 @@
 # * it is always possible to create a plan so that there are no three identical consecutive filters.
 
 
+def solution(S):
+    n = len(S)
+    if n == 1:
+        return 'a' if S == '?' else S
+
+    question_marks = find_question_maks(S)
+    S_options = [S]
+    while question_marks:
+        i = question_marks.pop()
+        new_options = []
+        for s in S_options:
+            s_a = s[:i] + 'a' + s[i + 1:]
+            if is_s_still_valid(s_a, i):
+                new_options.append(s_a)
+
+            s_b = s[:i] + 'b' + s[i + 1:]
+            if is_s_still_valid(s_b, i):
+                new_options.append(s_b)
+        S_options = new_options
+    return S_options[0]
+
+
+def is_s_still_valid(s, i):
+    n = len(s)
+    if i == 0:
+        cntr = {}
+        for i in range(3):
+            cntr[s[i]] = cntr.get(s[i], 0) + 1
+        return len(cntr) > 1
+    elif i == (n - 1):
+        cntr = {}
+        for i in range(n - 3, n):
+            cntr[s[i]] = cntr.get(s[i], 0) + 1
+        return len(cntr) > 1
+    elif s[i] == s[i - 1] and s[i] == s[i + 1]:
+        return False
+    elif is_valid_index(i - 2, n) and s[i - 1] == s[i - 2] and s[i - 1] == s[i]:
+        return False
+    elif is_valid_index(i + 2, n) and s[i + 1] == s[i + 2] and s[i + 1] == s[i]:
+        return False
+    return True
+
+
+def find_question_maks(S):
+    question_marks = []
+    for i in range(len(S)):
+        if S[i] == '?':
+            question_marks.append(i)
+    return question_marks
+
+
+def is_valid_index(i, n):
+    return 0 <= i < n
+
 
