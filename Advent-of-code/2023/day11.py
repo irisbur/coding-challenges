fcd00ab679e4d3c735fcb8692906eb9093173757
irisbur <irisburmi@gmail.com>
2024-08-28 15:17:39 +0300

    day11: wip.

--- Advent-of-code/2023/day11.py
+++ Advent-of-code/2023/day11.py
@@ -1,63 +1,81 @@
 
 # write a naive solution and then improve it
-
+from collections import deque
 
 def find_sum_galaxies_length():
     with open("input.txt", "r") as input_file:
         grid = [list(line.strip()) for line in input_file.readlines()]
         # grid, galaxies, expended_rows, expended_cols = find_galaxies(grid)
         sum_length = 0
-        expend_grid(grid)
-        # for i, galaxy in enumerate(galaxies):
-        #     dist_mat = find_dists_in_grid(grid, galaxy, expended_rows, expended_cols)
+        expended_grid = expend_grid(grid)
+        updated_grid, galaxies = find_galaxies(expended_grid)
+
+        for i, galaxy in enumerate(galaxies):
+            dist_mat = find_dists_in_grid(grid, galaxy)
         #     print(dist_mat)
 
 
-def find_dists_in_grid(grid, start, expended_rows, expended_cols):
-    dist_mat = [[-1 for _ in range(len(grid[0]))] for _ in range(len(grid))]  # visited list
+def find_dists_in_grid(grid, start):
+    dist_mat = [[-1 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+    n, m = len(grid), len(grid[0])
     si, sj = start
     dist_mat[si][sj] = 0
     directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    queue = deque([start])
+
+    while queue:
+        i, j = deque.popleft()
+
+        for di, dj in directions:
+            ni, nj = i + di, j + dj
+            if is_valid_index(n, m, ni, nj) and dist_mat == -1:
+                dist_mat[ni][nj] = 1 + grid[i][j]
+            elif is_valid_index(n, m, ni, nj) and dist_mat != -1:
+                dist_mat[ni][nj] = min(1 + grid[i][j], dist_mat[ni][nj])
+    for line in dist_mat:
+        print(''.join(line))
+    return dist_mat
+
+
+
+
+
+def is_valid_index(n, m, i, j):
+    return 0 <= i < n and 0 <= j < m
 
 
 def expend_grid(grid):
-    galaxies = []
-    # galaxies_count = 0
     expended_rows, expended_cols = {x for x in range(len(grid))}, {x for x in range(len(grid[0]))}
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == "#":
-                galaxies.append((i, j))
-                # galaxies_count += 1
-                # grid[i][j] = galaxies_count
+
                 if i in expended_rows:
                     expended_rows.remove(i)
                 if j in expended_cols:
                     expended_cols.remove(j)
 
-    # expend rows
+    for i, idx in enumerate(expended_rows):
+        grid.insert(i + idx, ['.' for _ in range(len(grid[0]))])
 
-    for i,idx in enumerate(expended_rows):
-        grid = grid[:idx + i] + [['.' for _ in range(len(grid[0]))]] + grid[idx + i:]
+    for i, idx in enumerate(expended_cols):
+        for row in grid:
+            row.insert(i + idx, '.')
 
-    # expend cols
-    helper = transpose(grid)
-    for i,idx in enumerate(expended_cols):
-        helper = helper[:idx + i] + [['.' for _ in range(len(grid))]] + helper[idx +i:]
-    grid = transpose(helper)
-
-    for line in grid:
-        print(''.join(line))
-
-    return grid, galaxies, expended_rows, expended_cols
+    return grid
 
 
-def transpose(grid): # grid m x n
-    transposed_grid = [[0 for _ in range(len(grid))] for _ in range(len(grid[0]))] # grid_t m x n
-    for i in range(len(transposed_grid)):
-        for j in range(len(transposed_grid[0])):
-            transposed_grid[i][j] = grid[j][i]
-    return transposed_grid
+def find_galaxies(grid):
+    galaxies = []
+    galaxies_count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == "#":
+                galaxies_count += 1
+                grid[i][j] = galaxies_count
+                galaxies.append((i, j))
+    return grid, galaxies
+
 
 
 print(find_sum_galaxies_length())
