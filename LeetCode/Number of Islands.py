c9987511c606ee1a17f8f45eb339970727b525a9
irisbur <irisburmi@gmail.com>
2024-09-28 21:24:30 +0300

    LeetCode: improve number of islands.

--- LeetCode/Number of Islands.py
+++ LeetCode/Number of Islands.py
@@ -1,43 +1,33 @@
+from collections import deque
 from typing import List
 
 
 def num_islands(grid: List[List[str]]) -> int:
-    num_islands = 0
+    if not grid:
+        return 0
+
+    islands_count = 0
     n, m = len(grid), len(grid[0])
     directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    ones = find_all_ones(grid)
+    visited = set()
 
-    if not ones:
-        return 0
-    fi, fj = ones[0]
-    current_island = [(fi, fj)]
-    visited = {(fi, fj)}
-    while ones:
-        while current_island:
-            ci, cj = current_island.pop()
-            ones.remove((ci, cj))
+    def bfs(i, j):
+        q = deque([(i, j)])
+        visited.add((i, j))
+
+        while q:
+            ci, cj = q.popleft()
+
             for di, dj in directions:
                 ni, nj = ci + di, cj + dj
-                if is_valid_index(ni, nj, n, m) and (ni, nj) not in visited:
+                if (0 <= ni < n) and (0 <= nj < m) and (grid[ni][nj] == '1') and (ni, nj) not in visited:
                     visited.add((ni, nj))
-                    if grid[ni][nj] == '1':
-                        current_island.append((ni, nj))
-        num_islands += 1
-        if ones:
-            current_island = [ones[0]]
-            visited.add(ones[0])
+                    q.append((ni, nj))
 
-    return num_islands
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == "1" and (i, j) not in visited:
+                bfs(i, j)
+                islands_count += 1
 
-
-def is_valid_index(i, j, n, m):
-    return 0 <= i < n and 0 <= j < m
-
-
-def find_all_ones(grid):
-    ones = []
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == '1':
-                ones.append((i, j))
-    return ones
+    return islands_count
