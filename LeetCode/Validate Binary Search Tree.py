c7a5038a9e2d84cebc26c9b67b4a1029ff2340ca
irisbur <irisburmi@gmail.com>
2024-10-06 22:16:56 +0300

    LeetCode: improve solution.

--- LeetCode/Validate Binary Search Tree.py
+++ LeetCode/Validate Binary Search Tree.py
@@ -26,3 +26,18 @@ def traverse_tree(root):
     left = traverse_tree(root.left)
     right = traverse_tree(root.right)
     return left + [root.val] + right
+
+
+# DFS solution
+def isValidBST(root: Optional[TreeNode]) -> bool:
+    def dfs(node, l, r):
+        if not node:
+            return True
+
+        if l < node.val < r:
+            left = dfs(node.left, l, node.val)
+            right = dfs(node.right, node.val, r)
+            return left and right
+        return False
+
+    return dfs(root, -2 ** 31 - 1, 2 ** 31)
