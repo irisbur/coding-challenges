b9fe8dd9eb30eb337c407572ce35c0065b3fd7ef
irisbur <irisburmi@gmail.com>
2024-11-03 21:27:33 +0200

    AOC 2015: solve day 9 p1.

---
+++ Advent-of-code/2015/day 9.py
@@ -0,0 +1,38 @@
+import math
+
+def all_paths_perms(places):
+    perms = [[places[0]]]
+    for place in places[1:]:
+        new_perms = []
+        for p in perms:
+            for i in range(len(p)+1):
+                cur = p.copy()
+                cur.insert(i, place)
+                new_perms.append(cur)
+        perms = new_perms
+    return perms
+
+
+def main():
+    with open('input.txt') as f:
+        lines = f.readlines()
+        adjacency_list = dict()
+        places = []
+        for line in lines:
+            line = line.split()
+            if line[0] not in places:
+                places.append(line[0])
+            if line[2] not in places:
+                places.append(line[2])
+            adjacency_list[tuple(sorted((line[0], line[2])))] = int(line[4])
+        all_perms = all_paths_perms(places)
+        lowest_cost = math.inf
+        for path in all_perms:
+            cur_cost = 0
+            for i in range(len(places) - 1):
+                cur_cost += adjacency_list[tuple(sorted((path[i], path[i+1])))]
+            lowest_cost = min(lowest_cost, cur_cost)
+        return lowest_cost
+
+
+print(main())
