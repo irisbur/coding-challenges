26aea068f1a0ae69bc227f694a140bc932a6e066
irisbur <irisburmi@gmail.com>
2024-11-11 22:21:28 +0200

    AoC 2015: solve day 12 part 2.

--- Advent-of-code/2015/day 12.py
+++ Advent-of-code/2015/day 12.py
@@ -1,3 +1,5 @@
+import json
+
 
 def find_all_numbers(text):
     num_sum = 0
@@ -19,10 +21,51 @@ def find_all_numbers(text):
     return num_sum
 
 
+def count_in_list(json_list):
+    count = 0
+    for v in json_list:
+        if isinstance(v, dict):
+            count += count_in_dict(v)
+        elif isinstance(v, list):
+            count += count_in_list(v)
+        elif isinstance(v, int):
+            count += v
+        elif v.isdigit():
+            count += int(v)
+    return count
+
+
+def count_in_dict(json_dict):
+    count = 0
+    for k, v in json_dict.items():
+        if k == 'red' or v == 'red':
+            return 0
+        elif isinstance(v, dict):
+            count += count_in_dict(v)
+        elif isinstance(v, list):
+            count += count_in_list(v)
+        elif isinstance(v, int):
+            count += v
+        elif v.isdigit():
+            count += int(v)
+    return count
+
+
+def count_in_json(input_json):
+    count = 0
+    if isinstance(input_json, dict):
+        count += count_in_dict(input_json)
+    elif isinstance(input_json, list):
+        count += count_in_list(input_json)
+    return count
+
+
 def main():
     with (open('input.txt') as f):
-        lines = f.readlines()
-        print(find_all_numbers(lines[0]))
+        json_string = f.readlines()[0]
+        js = json.loads(json_string)
+
+        print(count_in_json(js))
 
 
 if __name__ == '__main__':
