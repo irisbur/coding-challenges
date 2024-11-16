d147d19fc05fcbc4956db04e174684cd43920c21
irisbur <irisburmi@gmail.com>
2024-11-16 21:29:49 +0200

    AoC 2015: solve day 14.

--- Advent-of-code/2015/day 14.py
+++ Advent-of-code/2015/day 14.py
@@ -11,18 +11,34 @@ def calculate_distance(v_fly, t_fly, t_rest, total_t):
     return distance
 
 
+def argmax(dists):
+    best = max(dists)
+    return [i for i, d in enumerate(dists) if d == best]
+
+
+def calculate_points(deer, total_time):
+    deer_points = [0] * len(deer.keys())
+    deer_dists = [0] * len(deer.keys())
+    for t in range(1, total_time+1):
+        for i, c_deer in enumerate(deer):
+            v_fly, t_fly, t_rest = deer[c_deer]
+            dist = calculate_distance(v_fly, t_fly, t_rest, t)
+            deer_dists[i] = dist
+        indices = argmax(deer_dists)
+        for j in indices:
+            deer_points[j] += 1
+    print(max(deer_points))
+
+
 def main():
     with (open('input.txt') as f):
         lines = f.readlines()
-        win_dist = 0
+        deer = dict()
         for line in lines:
             line = line.split()
-            v_fly, t_fly, t_rest = int(line[3]), int(line[6]), int(line[-2])
-            d = calculate_distance(v_fly, t_fly, t_rest, 2503)
-            print(d)
-            win_dist = max(win_dist, d)
-
-        print(win_dist)
+            name = line[0]
+            deer[name] = (int(line[3]), int(line[6]), int(line[-2]))
+        calculate_points(deer, 2503)
 
 
 if __name__ == '__main__':
