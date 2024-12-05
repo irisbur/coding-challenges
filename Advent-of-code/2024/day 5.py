ee3be5c2fe0ae95a2b23b3a931cc926c38a7449e
irisbur <irisburmi@gmail.com>
2024-12-05 23:47:47 +0200

    AoC 2024: solve day 5 part 2.

--- Advent-of-code/2024/day 5.py
+++ Advent-of-code/2024/day 5.py
@@ -1,26 +1,18 @@
 
 
-def parse_input_for_part_1():
-    with (open('input.txt') as f):
+def parse_input_for_part_1(file_name='input.txt', rule_lines=21, update_start=22):
+    with (open(file_name) as f):
         lines = f.readlines()
-        rules = dict()
-        v = set()
-        for line in lines[:21]:
-            line = line.split('|')
-            a, b = int(line[0]), int(line[1])
-            v.add(a)
-            v.add(b)
-            if b in rules:
-                rules[b].append(a)
-            else:
-                rules[b] = [a]
+        rules = {}
+        vertices = set()
 
-        updates = [line.split(',') for line in lines[22:]]
-        new_updates = []
-        for update in updates:
-            new_updates.append([int(n) for n in update])
+        for line in lines[:rule_lines]:
+            a, b = map(int, line.split('|'))
+            vertices.update([a, b])
+            rules.setdefault(b, []).append(a)
 
-    return rules, list(v), new_updates
+        updates = [list(map(int, line.split(','))) for line in lines[update_start:]]
+        return rules, sorted(vertices), updates
 
 def is_update_correct(rules, update):
     is_correct = True
@@ -39,34 +31,52 @@ def count_correct_middle_pages():
             middles_sum += update[len(update) // 2]
     return middles_sum
 
-def sort_helper(ve, v, visited, stack, rules):
-    visited[v] = True
+def sort_helper(update, v, visited, stack, edges):
+    visited[update.index(v)] = True
 
-    for i in rules[v]:
-        if not visited[i]:
-            sort_helper(ve, i, visited, stack)
+    if v in edges:
+        for u in edges[v]:
+            if u in update and not visited[update.index(u)]:
+                sort_helper(update, u, visited, stack, edges)
 
     stack.insert(0, v)
 
-# sort topological
-def fix_update(rules, update):
+# topological sort
+def fix_update(update, edges):
     visited = [False] * len(update)
     stack = []
 
     for i in range(len(update)):
         if not visited[i]:
-            sort_helper(update, i, visited, stack, rules)
+            sort_helper(update, update[i], visited, stack, edges)
 
     return stack
 
 
+def parse_input_for_part_2(file_name='input.txt', rule_lines=1176, update_start=1177):
+    with (open(file_name) as f):
+        lines = f.readlines()
+        vertices = set()
+        rules = {}
+        edges = {}
+
+        for line in lines[:rule_lines]:
+            a, b = map(int, line.split('|'))
+            vertices.update([a, b])
+            edges.setdefault(a, []).append(b)
+            rules.setdefault(b, []).append(a)
+
+        updates = [list(map(int, line.split(','))) for line in lines[update_start:]]
+        return vertices, edges, rules, updates
+
 def count_wrong_middle_pages():
-    rules, ve, updates = parse_input()
+    vertices, edges, rules, updates = parse_input_for_part_2()
     middles_sum = 0
     for update in updates:
         if not is_update_correct(rules, update):
-            sorted_update = fix_update(rules, update)
-            middles_sum += update[len(sorted_update) // 2]
+            sorted_update = fix_update(update, edges)
+            print(sorted_update)
+            middles_sum += sorted_update[len(sorted_update) // 2]
     return middles_sum
 
 
