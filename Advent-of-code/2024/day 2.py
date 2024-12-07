ef9de906ffb08060dd84b313886bebfa9a8ff5e2
irisbur <irisburmi@gmail.com>
2024-12-07 18:02:51 +0200

    AoC 2024: solve day 2.

--- Advent-of-code/2024/day 2.py
+++ Advent-of-code/2024/day 2.py
@@ -11,25 +11,28 @@ def sign(x):
         return 1
     return 0 if x  == 0 else -1
 
-def is_report_safe(report, is_pd):
+def is_report_safe(report):
     diff = 0
-    is_safe = True
     for i in range(1, len(report)):
         cur_diff = report[i] - report[i - 1]
-        if diff != 0 and sign(diff) != sign(cur_diff):
-            is_safe = False
-        elif not 1 <= abs(cur_diff) <= 3:
-            is_safe = False
+        if diff != 0 and (sign(diff) != sign(cur_diff)) or not 1 <= abs(cur_diff) <= 3:
+            return False
         diff = cur_diff
-
-    return is_safe
+    return True
 
 
 def safe_reports_count(reports):
     count = 0
     for report in reports:
-        if is_report_safe(report, False):
+        is_safe = is_report_safe(report)
+        if is_safe:
             count += 1
+        else:
+            for i in range(len(report)):
+                if is_report_safe(report[:i] + report[i+1:]):
+                    count += 1
+                    break
+
     return count
 
 if __name__ == '__main__':
